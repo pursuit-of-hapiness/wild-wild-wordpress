3814b4053cffcc3fa93c2ca7fed6074b8a27a396
Shadows666 <lifeloveregretsxe@gmail.com>
2016-07-20 17:06:48 +0100

    Update post to contain IDs

--- src/helpers/updatecontent.js
+++ src/helpers/updatecontent.js
@@ -1,13 +1,13 @@
 const pgClient = require('./pgclient.js');
 
 const updateContent = (updatedPost, callback) => {
-  const title = updatedPost.title;
+  const id = updatedPost.id;
   const updatedContent = updatedPost.contentBody;
   const client = pgClient();
   client.query(`UPDATE content
                 SET content_body = $2
-                WHERE title = $1`,
-                [title, updatedContent],
+                WHERE id = $1`,
+                [id, updatedContent],
   (err, result) => {
     if (err) {
       console.log(err);
